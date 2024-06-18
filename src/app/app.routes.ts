3faf8ef22a494305426219900ffe7db7d6715cc0
Charmy <charmytseng0118@gmail.com>
2024-06-19 00:18:44 +0800

    測試建立新頁面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { PageComponent } from './Pages/page/page.component';
+import { OriginComponent } from './Pages/origin/origin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // 首頁
+  { path: 'origin', component: OriginComponent }, // 原 app.component
+  { path: 'page', component: PageComponent }, // 測試建立新頁面
+];
