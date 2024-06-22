203b84109eecc5fb47094347454b28072fdfed8f
Charmy <charmytseng0118@gmail.com>
2024-06-23 02:00:57 +0800

    if 判別式練習

---
+++ src/app/Pages/if-condition/if-condition.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 導入 CommonModule，*ngFor 需要，不然會產生警告
+import { HeaderComponent } from "../../Component/header/header.component";
+
+@Component({
+    selector: 'app-if-condition',
+    standalone: true,
+    templateUrl: './if-condition.component.html',
+    styleUrl: './if-condition.component.scss',
+    imports: [CommonModule, HeaderComponent]
+})
+export class IfConditionComponent {
+  /* if */
+  if_isVisible: boolean = true;
+
+  status: string = 'success';
+  setStatus(newStatus: string): void {
+    this.status = newStatus;
+  }
+
+  /* if else */
+  ifelse_isVisible: boolean = true;
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './Pages/home/home.component';
 import { PageComponent } from './Pages/page/page.component';
 import { OriginComponent } from './Pages/origin/origin.component';
 import { ForLoopComponent } from './Pages/for-loop/for-loop.component';
+import { IfConditionComponent } from './Pages/if-condition/if-condition.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // 首頁
   { path: 'origin', component: OriginComponent }, // 原 app.component
   { path: 'page', component: PageComponent }, // 測試建立新頁面
   { path: 'for-loop', component: ForLoopComponent }, // For 迴圈練習
+  { path: 'if-condition', component: IfConditionComponent }, // if 判別式練習
 ];
