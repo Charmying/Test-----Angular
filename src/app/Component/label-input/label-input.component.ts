f718289e4a3e96f0da9d98042e75d08ffa3b2072
Charmy <charmytseng0118@gmail.com>
2024-07-11 01:42:41 +0800

    修改 label-input component 並在 FormGroupComponent 中使用

--- src/app/Component/label-input/label-input.component.ts
+++ src/app/Component/label-input/label-input.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-label-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './label-input.component.html',
   styleUrl: './label-input.component.scss'
 })
@@ -11,4 +12,12 @@ export class LabelInputComponent {
   @Input() label: string = '';
   @Input() inputType: string = 'text';
   @Input() connectId: string = '';
+  @Input() form!: FormGroup; // 接受父組件傳入的 FormGroup
+  @Input() controlName: string = ''; // 控制元件的名稱，用於 FormGroup 的 formControlName
+
+  constructor() {}
+
+  get control() {
+    return this.form.get(this.controlName) as FormControl;
+  }
 }
--- src/app/Pages/form-group/form-group.component.ts
+++ src/app/Pages/form-group/form-group.component.ts
@@ -3,18 +3,33 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from "../../Component/header/header.component";
 import { SectionComponent } from "../../Component/section/section.component";
+import { LabelInputComponent } from "../../Component/label-input/label-input.component";
 
 @Component({
   selector: 'app-form-group',
   standalone: true,
   templateUrl: './form-group.component.html',
   styleUrl: './form-group.component.scss',
-  imports: [CommonModule, ReactiveFormsModule, HeaderComponent, SectionComponent] // 直接在這裡導入 ReactiveFormsModule，FormGroup 要用
+  imports: [CommonModule, ReactiveFormsModule, HeaderComponent, SectionComponent, LabelInputComponent] // 直接在這裡導入 ReactiveFormsModule，FormGroup 要用
+ // 直接在這裡導入 ReactiveFormsModule，FormGroup 要用
 })
 export class FormGroupComponent {
   headerTitle = 'FormGroup';
   section1Title = 'FormGroup 簡單範例 (在 constructor() 裡初始化)';
   section2Title = 'FormGroup 簡單範例 (用 非空斷言操作符(!) 在 ngOnInit() 裡初始化)';
+
+  name1 = 'Name:';
+  nameInputType1 = 'text';
+  nameControlName1 = 'name';
+  email1 = 'Email:';
+  emailInputType1 = 'text';
+  emailControlName1 = 'email';
+  name2 = 'Name:';
+  nameInputType2 = 'text';
+  nameControlName2 = 'name';
+  email2 = 'Email:';
+  emailInputType2 = 'text';
+  emailControlName2 = 'email';
 
   myForm1: FormGroup;
   myForm2!: FormGroup;
