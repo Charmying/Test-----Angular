b2beee19d2cbb355b96cf2cd907bd744bf2da18a
Charmy <charmytseng0118@gmail.com>
2025-03-14 00:24:29 +0800

    新增 app-form-input-text 共用元件

--- src/app/Pages/demo-page/demo-forms/demo-forms.component.ts
+++ src/app/Pages/demo-page/demo-forms/demo-forms.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormInputTextComponent } from '../../../shared/components/forms/form-input-text/form-input-text.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-forms',
   standalone: true,
   templateUrl: './demo-forms.component.html',
   styleUrl: './demo-forms.component.scss',
-  imports: [],
+  imports: [FormInputTextComponent, ButtonComponent],
 })
-export class DemoFormsComponent {}
+export class DemoFormsComponent {
+  /** FormGroup */
+  form!: FormGroup;
+  
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({
+      inputText_text: [''],
+      inputText_number: [''],
+      inputText_email: [''],
+      inputText_tel: [''],
+    });
+  }
+
+  /** 檢查 FormGroup */
+  click() {
+    console.log(this.form.value)
+  }
+}
---
+++ src/app/shared/components/forms/form-input-text/form-input-text.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-input-text',
+  templateUrl: './form-input-text.component.html',
+  styleUrls: ['./form-input-text.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+})
+export class FormInputTextComponent {
+  /** FormGroup */
+  @Input() form!: FormGroup;
+  /** input title */
+  @Input() title = '';
+  /** 類型 */
+  @Input() type: 'text' | 'number' | 'email' | 'tel' = 'text';
+  /** formControlName */
+  @Input() formControlName = '';
+  /** placeHolder */
+  @Input() placeHolder = '';
+}
