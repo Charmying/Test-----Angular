263ff20bebe00a81b9b6be5b8a2eeeb65b1c7674
Charmy <charmytseng0118@gmail.com>
2025-03-30 17:59:25 +0800

    把 test1 和 test2 的資料取出來

--- src/app/Pages/test-api/test1-and-test2-api-page/test1-and-test2-api-page.component.ts
+++ src/app/Pages/test-api/test1-and-test2-api-page/test1-and-test2-api-page.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { TestApiHeaderComponent } from '../shared/test-api-header/test-api-header.component';
 import { SectionComponent } from '../../../shared/components/test/section/section.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-test1-and-test2-api-page',
@@ -10,4 +11,35 @@ import { SectionComponent } from '../../../shared/components/test/section/sectio
   styleUrl: './test1-and-test2-api-page.component.scss',
   imports: [HeaderComponent, TestApiHeaderComponent, SectionComponent]
 })
-export class Test1AndTest2ApiPageComponent {}
+export class Test1AndTest2ApiPageComponent implements OnInit {
+  data1: any;
+  data2: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get('https://test-express-api-x0j9.onrender.com/test1').subscribe({
+      next: (response) => {
+        this.data1 = response;
+        console.log('API 資料:', this.data1);
+      },
+      error: (err) => {
+        console.error('獲取資料時發生錯誤:', err);
+      }
+    });
+
+    this.http.get('https://test-express-api-x0j9.onrender.com/test2').subscribe({
+      next: (response) => {
+        this.data2 = response;
+        console.log('API 資料:', this.data2);
+      },
+      error: (err) => {
+        console.error('獲取資料時發生錯誤:', err);
+      }
+    });
+  }
+}
