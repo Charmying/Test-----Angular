94a4c995e1d8d9f5e60ab363118f06a9d8e5dc03
Charmy <charmytseng0118@gmail.com>
2025-04-09 00:16:53 +0800

    整理 code & api service & test1 test2 API 資料顯示方式調整

--- src/app/Pages/test-api/test1-and-test2-api-page/test1-and-test2-api-page.component.ts
+++ src/app/Pages/test-api/test1-and-test2-api-page/test1-and-test2-api-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { TestApiHeaderComponent } from '../shared/test-api-header/test-api-header.component';
 import { SectionComponent } from '../../../shared/components/test/section/section.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { ApiService } from '../../../shared/service/api/api.service';
 import { finalize, tap } from 'rxjs'; // 使用 RxJS 的 tap 和 finalize 方法時需要
 import { forkJoin } from 'rxjs'; // 使用 forkJoin 方法時需要
@@ -12,35 +13,35 @@ import { forkJoin } from 'rxjs'; // 使用 forkJoin 方法時需要
   standalone: true,
   templateUrl: './test1-and-test2-api-page.component.html',
   styleUrl: './test1-and-test2-api-page.component.scss',
-  imports: [CommonModule, HeaderComponent, TestApiHeaderComponent, SectionComponent]
+  imports: [CommonModule, HeaderComponent, TestApiHeaderComponent, SectionComponent, ButtonComponent]
 })
 export class Test1AndTest2ApiPageComponent implements OnInit {
-  apiUrl1: string = 'https://test-express-api-x0j9.onrender.com/test1';
-  apiUrl2: string = 'https://test-express-api-x0j9.onrender.com/test2';
-  data1: any;
-  data2: any;
-  allData: any;
-  isLoading = true;
+  // apiUrl1: string = 'https://test-express-api-x0j9.onrender.com/test1';
+  // apiUrl2: string = 'https://test-express-api-x0j9.onrender.com/test2';
+  // data1: any;
+  // data2: any;
+  // allData: any;
+  // isLoading = true;
 
-  constructor(private apiService: ApiService) {}
+  // constructor(private apiService: ApiService) {}
 
   /**
    * 使用 async await 方法
    * 使用 getData
    * 需要 return Promise<any>
    */
-  async ngOnInit() {
-    try {
-      this.data1 = await this.apiService.getData(this.apiUrl1);
-      this.data2 = await this.apiService.getData(this.apiUrl2);
+  // async ngOnInit() {
+  //   try {
+  //     this.data1 = await this.apiService.getData(this.apiUrl1);
+  //     this.data2 = await this.apiService.getData(this.apiUrl2);
 
-      this.allData = [...this.data1, ...this.data2];
-    } catch (err) {
-      console.error('獲取資料時發生錯誤:', err);
-    } finally {
-      this.isLoading = false;
-    }
-  }
+  //     this.allData = [...this.data1, ...this.data2];
+  //   } catch (err) {
+  //     console.error('獲取資料時發生錯誤:', err);
+  //   } finally {
+  //     this.isLoading = false;
+  //   }
+  // }
 
   /** ==================================================================================================== */
 
@@ -149,4 +150,37 @@ export class Test1AndTest2ApiPageComponent implements OnInit {
   //     complete: () => this.isLoading = false
   //   });
   // }
+
+  /** ==================================================================================================== */
+
+  /** API 網址 */
+  apiUrl: string = 'https://test-express-api-x0j9.onrender.com';
+  /** 預設顯示 test1 */
+  currentDatabase = 'test1';
+  /** 表格資料 */
+  data: any[] = [];
+  /** 判斷是否載入完成 */
+  isLoading = true;
+
+  constructor(private apiService: ApiService) {}
+
+  async ngOnInit() {
+    this.fetchData();
+  }
+
+  /** 取得 API 資料 */
+  async fetchData() {
+    this.isLoading = true;
+    try {
+      this.data = await this.apiService.get(`${this.apiUrl}/${this.currentDatabase}`);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  /** 按鈕 click 事件切換 test1 & test2 */
+  switchDatabase(database: string) {
+    this.currentDatabase = database;
+    this.fetchData();
+  }
 }
--- src/app/shared/service/api/api.service.ts
+++ src/app/shared/service/api/api.service.ts
@@ -11,26 +11,26 @@ export class ApiService {
    * return 的是 Promise<any>
    * async await 方法
    */
-  constructor(private http: HttpClient) {}
+  // constructor(private http: HttpClient) {}
 
-  // 封裝 GET 請求
-  async getData(url: string): Promise<any> {
-    try {
-      return await firstValueFrom(this.http.get(url));
-    } catch (err) {
-      throw err;
-    }
-  }
+  /** 封裝 GET 請求 */
+  // async getData(url: string): Promise<any> {
+  //   try {
+  //     return await firstValueFrom(this.http.get(url));
+  //   } catch (err) {
+  //     throw err;
+  //   }
+  // }
 
-  /** 直接取 test1 api */
-  async getTest1Data(): Promise<any> {
-    return this.getData('https://test-express-api-x0j9.onrender.com/test1');
-  }
+  // /** 直接取 test1 api */
+  // async getTest1Data(): Promise<any> {
+  //   return this.getData('https://test-express-api-x0j9.onrender.com/test1');
+  // }
 
-  /** 直接取 test2 api */
-  async getTest2Data(): Promise<any> {
-    return this.getData('https://test-express-api-x0j9.onrender.com/test2');
-  }
+  // /** 直接取 test2 api */
+  // async getTest2Data(): Promise<any> {
+  //   return this.getData('https://test-express-api-x0j9.onrender.com/test2');
+  // }
 
   /** ==================================================================================================== */
 
@@ -40,7 +40,7 @@ export class ApiService {
    */
   // constructor(private http: HttpClient) {}
 
-  // // 封裝 GET 請求
+  /** 封裝 GET 請求 */
   // getData(url: string): Observable<any> {
   //   return this.http.get(url);
   // }
@@ -54,4 +54,24 @@ export class ApiService {
   // getTest2Data(): Observable<any> {
   //   return this.getData('https://test-express-api-x0j9.onrender.com/test2');
   // }
+
+  /** ==================================================================================================== */
+
+  constructor(private http: HttpClient) {}
+  
+  async get<T>(url: string): Promise<T> {
+    return firstValueFrom(this.http.get<T>(url));
+  }
+
+  async post<T>(url: string, body: any): Promise<T> {
+    return firstValueFrom(this.http.post<T>(url, body));
+  }
+
+  async put<T>(url: string, body: any): Promise<T> {
+    return firstValueFrom(this.http.put<T>(url, body));
+  }
+
+  async delete<T>(url: string): Promise<T> {
+    return firstValueFrom(this.http.delete<T>(url));
+  }
 }
