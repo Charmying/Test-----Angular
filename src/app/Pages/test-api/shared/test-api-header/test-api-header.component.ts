4df802e62d220ca45f434a5393d239845e2b820a
Charmy <charmytseng0118@gmail.com>
2025-04-20 02:55:40 +0800

    href 改成 routerLink

--- src/app/Pages/test-api/shared/test-api-header/test-api-header.component.ts
+++ src/app/Pages/test-api/shared/test-api-header/test-api-header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-test-api-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './test-api-header.component.html',
   styleUrl: './test-api-header.component.scss'
 })
