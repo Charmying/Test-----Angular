abfaf133603e2cb15ab4d82a03586477a5b5283b
Charmy <charmytseng0118@gmail.com>
2025-04-27 00:17:50 +0800

    QR Code 點餐系統 user 頁加入圖片 & 稍微切版

--- src/app/pages/test-api/qrcode-order/shared/menu-obj.ts
+++ src/app/pages/test-api/qrcode-order/shared/menu-obj.ts
@@ -1,11 +1,39 @@
 /** menu object */
 
 export const Menu = [
-  { name: '瑪格麗特 (五辛奶素)', price: 240 },
-  { name: '拿坡里臘腸', price: 320 },
-  { name: '芝麻葉生火腿', price: 350 },
-  { name: '松露野菇 (奶素)', price: 320 },
-  { name: '櫛瓜鮮蝦', price: 320 },
-  { name: '青醬舒肥雞', price: 320 },
-  { name: '起司四重奏', price: 300 }
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '瑪格麗特 (五辛奶素)',
+    price: 240
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '拿坡里臘腸',
+    price: 320
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '芝麻葉生火腿',
+    price: 350
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '松露野菇 (奶素)',
+    price: 320
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '櫛瓜鮮蝦',
+    price: 320
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '青醬舒肥雞',
+    price: 320
+  },
+  {
+    photo: 'assets/images/menu.jpg',
+    name: '起司四重奏',
+    price: 300
+  }
 ]
